a69c537408bd56a075596ff70d10d8c31f2f7445
张仕坤 <shikun.zhang@dreamsmart.com>
2025-03-21 17:59:24 +0800

    fix: bug fix

--- src/app/posts/(post)/layout.tsx
+++ src/app/posts/(post)/layout.tsx
@@ -1,42 +1,6 @@
-import type { Metadata } from 'next'
 import './mdx.css'
 import { TableOfContents } from '@/components/TableOfContents'
 import { MDXContent } from '@/components/MDXContent'
-import { postsConfig } from '../../../../config'
-
-interface PageParams {
-  slug?: string
-}
-
-export async function generateMetadata({ params }: { params: PageParams }): Promise<Metadata> {
-  // 从 URL 路径中获取当前文章的 slug
-  const slug = params.slug || ''
-
-  // 在 postsConfig 中查找对应的文章
-  const post = postsConfig.find((post) => {
-    const postSlug = post.href.split('/').pop()
-    return postSlug === slug
-  })
-
-  if (!post) {
-    return {
-      title: 'Blog Post',
-      description: 'A blog post'
-    }
-  }
-
-  return {
-    title: `${post.title} | Imber's Blog`,
-    description: post.title,
-    openGraph: {
-      title: post.title,
-      description: post.title,
-      type: 'article',
-      authors: ['Imber'],
-      publishedTime: post.date
-    }
-  }
-}
 
 export default function RootLayout({
   children
