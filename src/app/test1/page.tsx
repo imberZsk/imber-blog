6e0cebbed62f22d5f4201905108dde448e5fbfb8
imber <shikun.zhang@dreamsmart.com>
2025-06-13 14:41:53 +0800

    chore: test

--- src/app/test1/page.tsx
+++ src/app/test1/page.tsx
@@ -1,12 +1,11 @@
 import Image from 'next/image'
 import { PADDING_TOP } from '../const'
 import { cn } from '@/lib/utils'
-import DemoImage from './images/avatar.jpg'
 
 const Test = () => {
   return (
     <div className={cn(PADDING_TOP)}>
-      <Image src={DemoImage} alt="test" />
+      <Image src="/avatar.jpg" width={100} height={100} alt="test" />
     </div>
   )
 }
--- src/app/test2/page.tsx
+++ src/app/test2/page.tsx
@@ -1,11 +1,12 @@
 import Image from 'next/image'
 import { PADDING_TOP } from '../const'
 import { cn } from '@/lib/utils'
+import DemoImage from './images/avatar.jpg'
 
 const Test = () => {
   return (
     <div className={cn(PADDING_TOP)}>
-      <Image src="/avatar.jpg" alt="test" />
+      <Image src={DemoImage} alt="test" />
     </div>
   )
 }
