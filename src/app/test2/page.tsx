fc9d4f503fccd33bb565f2c9be467287ec31a264
imber <shikun.zhang@dreamsmart.com>
2025-06-13 14:37:29 +0800

    chore: test

--- src/app/test2/page.tsx
+++ src/app/test2/page.tsx
@@ -5,7 +5,7 @@ import { cn } from '@/lib/utils'
 const Test = () => {
   return (
     <div className={cn(PADDING_TOP)}>
-      <Image src="/avatar.jpg" alt="test" width={100} height={100} />
+      <Image src="/avatar.jpg" alt="test" />
     </div>
   )
 }
