425e4af0dae5e114a1da70aedc446ed324e20400
imber <shikun.zhang@dreamsmart.com>
2025-06-23 14:26:50 +0800

    feat: [posts] 文章背景线采用定位防止滚动影响体验

--- src/app/posts/[slug]/layout.tsx
+++ src/app/posts/[slug]/layout.tsx
@@ -8,19 +8,20 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <div
-      className="mx-auto h-full w-full bg-[#191a1a]"
-      style={
-        {
-          '--color': 'rgba(114, 114, 114, 0.2)',
-          backgroundImage: `
+    <div className="relative mx-auto h-full w-full bg-[#191a1a]">
+      <div
+        className="fixed top-0 left-0 mx-auto h-screen w-full"
+        style={
+          {
+            '--color': 'rgba(114, 114, 114, 0.2)',
+            backgroundImage: `
             linear-gradient(0deg, transparent 24%, var(--color) 25%, var(--color) 26%, transparent 27%, transparent 74%, var(--color) 75%, var(--color) 76%, transparent 77%, transparent),
             linear-gradient(90deg, transparent 24%, var(--color) 25%, var(--color) 26%, transparent 27%, transparent 74%, var(--color) 75%, var(--color) 76%, transparent 77%, transparent)
           `,
-          backgroundSize: '55px 55px'
-        } as React.CSSProperties
-      }
-    >
+            backgroundSize: '55px 55px'
+          } as React.CSSProperties
+        }
+      ></div>
       <div className="relative mx-auto max-w-7xl">
         <TableOfContents />
         <MDXContent>{children}</MDXContent>
