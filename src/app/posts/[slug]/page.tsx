4d22c89027484f1a7a77f07927d3a4304bfc1fa2
imber <a1157911285@163.com>
2025-09-08 00:18:32 +0800

    feat: react-source

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -24,6 +24,7 @@ export function generateStaticParams() {
     { slug: 'tailwind' },
     { slug: 'typescript' },
     { slug: 'react' },
+    { slug: 'react-source' },
     { slug: 'editor' },
     { slug: 'webpack' }
   ]
