b0b0f084f0bde9eb332d451988762bbf38374f38
imber <a1157911285@163.com>
2025-10-02 21:28:08 +0800

    chore: 不管画廊页错误信息

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -3,22 +3,22 @@ import { fetchGitHubImages } from '@/services/gallery'
 import Waterfall from './waterfall'
 
 const Page = async () => {
-  const { data, error } = await fetchGitHubImages()
+  const { data } = await fetchGitHubImages()
 
   return (
     <div className={cn('mx-auto px-5 pt-32')}>
       {/* 错误状态 */}
-      {error && <div>请求失败{JSON.stringify(error)}</div>}
+      {/* {error && <div>请求失败{JSON.stringify(error)}</div>} */}
 
       {/* 图片网格 */}
-      {!error && (
-        <>
-          {/* 图片列表 */}
-          <Waterfall data={data} />
+      {/* {!error && (
+        <> */}
+      {/* 图片列表 */}
+      <Waterfall data={data} />
 
-          {/* 空状态 - 暂时不考虑 */}
-        </>
-      )}
+      {/* 空状态 - 暂时不考虑 */}
+      {/* </> */}
+      {/* )} */}
     </div>
   )
 }
