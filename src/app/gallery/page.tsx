06d8463dd68d3b62c0081ba9a137a59d3960267c
imber <a1157911285@163.com>
2025-10-11 17:01:58 +0800

    chore: 暂时关闭 gallery 页面

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -1,24 +1,23 @@
 import { cn } from '@/lib/utils'
-import { fetchGitHubImages } from '@/services/gallery'
-import Waterfall from './waterfall'
+// import { fetchGitHubImages } from '@/services/gallery'
+// import Waterfall from './waterfall'
 
 const Page = async () => {
-  const { data } = await fetchGitHubImages()
+  // const { data, error } = await fetchGitHubImages()
 
   return (
-    <div className={cn('mx-auto px-5 pt-32')}>
+    <div className={cn('mx-auto flex items-center justify-center px-5 pt-32 text-center text-red-500')}>
+      <div>
+        <p> 暂时关闭 gallery 页面，需要修改</p>
+        <p> TOOD: 之前图片放 Github 老是有点问题，或者网速太慢，准备修改</p>
+      </div>
       {/* 错误状态 */}
-      {/* {error && <div>请求失败{JSON.stringify(error)}</div>} */}
-
-      {/* 图片网格 */}
+      {/* {error && <div>请求失败</div>} */}
       {/* {!error && (
-        <> */}
-      {/* 图片列表 */}
-      <Waterfall data={data} />
-
-      {/* 空状态 - 暂时不考虑 */}
-      {/* </> */}
-      {/* )} */}
+        <>
+          <Waterfall data={data} />
+        </>
+      )} */}
     </div>
   )
 }
