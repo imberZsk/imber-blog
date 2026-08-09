90e1af623d6f36b5ce512dcd734dcb3d660b3fea
imber <a1157911285@163.com>
2026-08-09 13:14:07 +0800

    chore: remove Strapi integration

--- src/services/friends/index.ts
+++
@@ -1,8 +0,0 @@
-import { strapiService } from '@/services'
-
-export async function fetchFriends() {
-  return await strapiService.request('/api/friends?populate=avatar', {
-    next: { revalidate: 60 * 5 }
-    // GET 不用带Token
-  })
-}
